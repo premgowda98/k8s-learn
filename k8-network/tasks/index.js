231b2e33d949309347739496510cb5b7c46671e2
Prem Kumar <premkumarmvks@gmail.com>
2024-03-14 15:52:27 +0530

    pod networking

--- k8-network/tasks/index.js
+++ k8-network/tasks/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express()
+app.use(bodyParser.json());
 
-app.get('/', (req,res)=>{
-	res.send(`<h1>Hello Guru, Code Changed</h1>`);
+app.get('/task', async (req,res)=>{
+
+	try {
+		const response = await axios.get(`http://${process.env.AUTH_ADDR}/auth`)
+		if (response.status===200){
+			return res.json({message: 'Task got and also auth verified'});
+		}
+		return res.json({message: 'Taskify'});
+	} catch {
+		return res.json({message: 'Something went wrong in tasks'});
+	}
 }
 );
 
